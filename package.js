64a9c0a9ddb5a95776f7335460eaef15d4c8906e
lugy90 <naitianzong@outlook.com>
2018-03-02 10:45:30 +0800

    add package.js

---
+++ package.js
@@ -0,0 +1,22 @@
+{
+    "name": "question_socket",
+    "version": "1.0.0",
+    "description": "websocket api for testing",
+    "main": "index.js",
+    "scripts": {
+        "start":"./node_modules/.bin/nodemon index.js"
+    },
+    "keywords": [
+        "question_socket"
+    ],
+    "author": "lugy90",
+    "license": "ISC",
+    "dependencies": {
+        "express": "4.16.2",
+        "url": "0.11.0",
+        "ws": "4.0.0"
+    },
+    "devDependencies": {
+        "nodemon": '1.15.1'
+    }
+}
